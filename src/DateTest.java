efe2dc1960e64aaf7a7f293519b2d6d7a1d0754b
msherb5 <msherbs5@gmail.com>
2020-10-07 23:06:45 -0400

    all changes

--- src/DateTest.java
+++ src/DateTest.java
@@ -1,4 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 class DateTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void isValid() {
+        Date test1 = new Date(2020, 8, 2);
+        Date test2 = new Date(1999, 2,30);
+
+        assertTrue(test1.isValid());
+        assertFalse(test2.isValid());
+    }
 }
--- src/MoneyMarketTest.java
+++ src/MoneyMarketTest.java
@@ -1,4 +1,37 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MoneyMarketTest {
-  
+
+    @Test
+    void monthlyInterest() {
+        Date date = new Date(2020, 8,2);
+        Account account = new MoneyMarket(date, 500.00, "Dover", "Ben");
+
+        assertEquals((500 * ((.0065) / 12)), account.monthlyInterest());
+
+    }
+
+    @Test
+    void monthlyFee() {
+        Date date = new Date(2020, 8,2);
+        Account less2500 = new MoneyMarket(date, 500.00, "Dover", "Ben");
+        Account greater2500 = new MoneyMarket(date, 3000.00, "Mama", "Joe");
+        Account withdrawals = new MoneyMarket(date, 500.00, "Draw", "With");
+
+        assertEquals(12, less2500.monthlyFee());
+        assertEquals(0,greater2500.monthlyFee());
+
+        AccountDatabase data = new AccountDatabase();
+        data.add(withdrawals);
+
+
+        for(int numWithdraw = 0; numWithdraw < 7; numWithdraw++){
+            data.withdrawal(withdrawals, 1);
+
+        }
+        assertEquals(12, withdrawals.monthlyFee());
+
+    }
 }
